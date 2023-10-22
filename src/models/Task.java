835d6b35a4dce0d88dc5ac662d39ed5eabce9ec8
qazxs <qazxs@192.168.10.102>
2023-10-23 04:33:15 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAlltasks",
+            query = "SELECT m FROM Task AS m ORDER By m.id DESC"
+            )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
